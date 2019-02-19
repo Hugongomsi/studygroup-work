55b754dd47a75dc8af341c565aa98c38bb91b791
hugo <45979673+Hugongomsi@users.noreply.github.com>
2019-02-19 02:17:17 +0200

    Create loop javascript.js

---
+++ loop javascript.js
@@ -0,0 +1,30 @@
+1)--THE SUM OF A NUMBER FROM [1] TO [100]
+let number=[1,2,3,4,5, 100]
+let sum=1
+for(let i=1; i<= 100; i++){
+  let x=[i+1];
+  console.log(`the SUM of number from [1] to [100] is: " ${i} is: ${x}`);
+ sum =sum+i
+ }
+ console.log(`now the total is: ${sum}`)
+ 
+ 2)--THE ODD NUMBER FROM [1] TO [100]
+ let number=[1,2,3,4,5, 100]
+ let sum=1
+ for(let i=1; i<= 100; i++){
+   let x=[2*i-1]*[2*i-1];
+   console.log(`the sum of square of odd number from [1] to [100] is: " ${i} is: ${x}`);
+  sum =sum+x
+  }
+   console.log(`now the total is: ${sum}`)
+   
+ 3)--THE EVEN NUMBER FROM [1] TO [100]
+let number=[1,2,3,4,5, 100]
+ let sum=1
+ for(let i=1; i<= 100; i++){
+   let x=[2*i]*[2*i];
+   console.log(`the sum of square of even number from [1] to [100] is: " ${i} is: ${x}`);
+  sum =sum+x
+  }
+   console.log(`now the total is: ${sum}`)
+ 
